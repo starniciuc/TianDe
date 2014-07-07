f8a0b0166b7f8887f29253a7f204711a72f5170b
phoenix <phoenix@mihai.Dlink>
2014-07-07 12:26:08 +0300

    slice all page

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,45 +1,60 @@
 function Calendar2(id, year, month) {
-	var Dlast = new Date(year, month + 1, 0).getDate(),
-			D = new Date(year, month, Dlast),
-			DNlast = new Date(D.getFullYear(), D.getMonth(), Dlast).getDay(),
-			DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
-			calendar = '<tr>',
-			month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-	if (DNfirst != 0) {
-		for (var i = 1; i < DNfirst; i++)
-			calendar += '<td>';
-	} else {
-		for (var i = 0; i < 6; i++)
-			calendar += '<td>';
-	}
-	for (var i = 1; i <= Dlast; i++) {
-		if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-			calendar += '<td class="today">' + i;
-		} else {
-			calendar += '<td>' + i;
-		}
-		if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
-			calendar += '<tr>';
-		}
-	}
-	for (var i = DNlast; i < 7; i++)
-		calendar += '<td>&nbsp;';
-	document.querySelector('#' + id + ' tbody').innerHTML = calendar;
-	document.querySelector('#' + id + ' thead td:nth-child(2)').innerHTML = month[D.getMonth()] + ' ' + D.getFullYear();
-	document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.month = D.getMonth();
-	document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.year = D.getFullYear();
-	if (document.querySelectorAll('#' + id + ' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
-		document.querySelector('#' + id + ' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
-	}
+    var Dlast = new Date(year, month + 1, 0).getDate(),
+            D = new Date(year, month, Dlast),
+            DNlast = new Date(D.getFullYear(), D.getMonth(), Dlast).getDay(),
+            DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
+            calendar = '<tr>',
+            month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+    if (DNfirst != 0) {
+        for (var i = 1; i < DNfirst; i++)
+            calendar += '<td>';
+    } else {
+        for (var i = 0; i < 6; i++)
+            calendar += '<td>';
+    }
+    for (var i = 1; i <= Dlast; i++) {
+        if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+            calendar += '<td class="today">' + i;
+        } else {
+            calendar += '<td>' + i;
+        }
+        if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
+            calendar += '<tr>';
+        }
+    }
+    for (var i = DNlast; i < 7; i++)
+        calendar += '<td>&nbsp;';
+    document.querySelector('#' + id + ' tbody').innerHTML = calendar;
+    document.querySelector('#' + id + ' thead td:nth-child(2)').innerHTML = month[D.getMonth()] + ' ' + D.getFullYear();
+    document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.month = D.getMonth();
+    document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.year = D.getFullYear();
+    if (document.querySelectorAll('#' + id + ' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
+        document.querySelector('#' + id + ' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
+    }
 }
-
 $(".btn-open").click(function() {
-        var mybtn = $(this);
-        var par = mybtn.parents(".blog-page").children(".collapse");
-        par.collapse("show");
+    var mybtn = $(this);
+    var par = mybtn.parents(".blog-page").children(".collapse");
+    par.collapse("show");
 //        par.on('shown.bs.collapse', function() {
 //            mybtn.click(function() {
 //                par.collapse('hide');
 //            });
 //        });
+});
+$(".btn-open-team").click(function() {
+    var mybtn = $(this);
+    var par = mybtn.parents(".blog-page").children(".about-empl").children(".collapse");
+    par.collapse("show");
+    $(mybtn.parents(".blog-page")).css("z-index", "10");
+    par.on('shown.bs.collapse', function() {
+        mybtn.click(function() {
+            par.collapse('hide');
+        });
     });
+    par.on("hidden.bs.collapse", function() {
+
+        $(mybtn.parents(".blog-page")).css("z-index", "1");
+
+    });
+});
